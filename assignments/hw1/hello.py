ec0fd78232419e2446eb1c0a73e090d0164e420b
eviefle <fleischmaned@g.cofc.edu>
2021-09-03 12:50:15 -0400

    completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,13 @@
+"""
+Name: evie fleischman
+hello.py
+
+Problem: This program solves the issue of the words, "hello, world!" not being on the screen before and now they are.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+    main()
